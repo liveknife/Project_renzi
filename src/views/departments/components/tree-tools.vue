1c853d5e2510a34a123941ff6a29f52e088a4fd8
makun <mk2268108824@gmail.com>
2022-09-20 20:32:59 +0800

    插槽

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-row type="flex" justify="space-between" align="middle" style="height: 40px;width:100%">
+    <!-- el-col 控制列 -->
+    <el-col>
+      <!-- 左侧内容 -->
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <!-- :span="4" 控制占据的份数 一行栅格默认分为24份 -->
+    <el-col :span="4">
+      <!-- 开启flex布局并且展示在末尾 -->
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manger }}</el-col>
+        <el-col>
+          <!-- 下拉菜单 -->
+          <el-dropdown>
+            <span>
+              操作 <i class="el-icon-arrow-down" />
+            </span>
+            <!-- 具名插槽 -->
+            <el-dropdown-menu slot="dropdown">
+              <!-- 下拉选项 -->
+              <el-dropdown-item>
+                添加子部门
+              </el-dropdown-item>
+              <!-- 取反 这里因为下面设置的isRoot默认值设置的是false 所以这里取反就是当它的值为true的时候就删除这两个选项 哪个插槽设置了 isRoot = true 就不显示,编辑和删除部门的选项 -->
+              <el-dropdown-item v-if="!isRoot">
+                编辑部门
+              </el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">
+                删除部门
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+      <!-- 右侧内容 -->
+    </el-col>
+  </el-row>
+</template>
+<script>
+
+export default {
+  // 接收父组件传过来的数据
+  props: {
+    treeNode: {
+      type: Object,
+      required: true // 必须要写的配置
+    },
+    // 备注: isRoot如果父组件中传递值了以传递过来的为主，如果没有传递，则以默认的为主
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,19 +1,69 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <!-- 组织架构内容- 头部 -- -->
+      <el-card class="tree-card">
+        <!-- 放置结构内容 -->
+        <!-- el-row 控制行 type="flex" 开启flex布局 justify="space-between" 主轴对齐方式: 两边对齐-->
+        <!-- 将它的节点设置为true 当 isRoot = true的时候会删除节点 -->
+        <!-- 给子组件传值,根节点这个插槽的是不需要展示编辑和删除部门的选项 所以设置isRoot状态是true 通过这个isRoot的状态配合v-if或v-show来进行显示和隐藏 -->
+        <TreeTools :tree-node="company" :is-root="true" />
+
+        <!-- 放一个 el-tree 树形组件-->
+        <!--属性组件默认只识别数据中的label属性和children属性，如果是其他属性，我们需要通过props属性进行一个配置-->
+        <!-- default-expand-all 将所有的节点默认展开 -->
+        <!-- <el-tree default-expand-all :data="departs" :props="defaultProps"> -->
+        <!-- 传入内容 插槽内容 会循环多次,有多少节点 就循环多次 -->
+        <!-- 目标: 自定义树形组件的节点内容,有多少个节点就会显示多少个节点的内容 -->
+        <!-- 作用域插槽 slot-scoped='{ obj }' 接收传递给插槽的数据 data 每个节点的数据对象-->
+        <!-- 另外一种写法: 用template包裹起来 没有解构 在子组件内需要scoped.属性名.属性值来进行显示渲染 -->
+        <!-- template的作用是模板占位符，可帮助我们包裹元素，但在循环过程当中，template不会被渲染到页面上 -->
+        <!-- <template slot-scope="scoped">
+            <TreeTools :tree-node="scoped" />
+          </template> -->
+        <el-tree default-expand-all :data="departs" :props="defaultProps">
+          <!-- 将数据解构出来,子组件内直接 属性名.属性值就可以显示 -->
+          <TreeTools slot-scope="{data}" :tree-node="data" />
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
 export default {
-
+  components: {
+    TreeTools
+  },
+  data() {
+    return {
+      company: {
+        name: '江苏传智播客教育科技股份有限公司',
+        manger: '负责人'
+      },
+      departs: [
+        { name: '总裁办',
+          manger: '朱元璋',
+          children: [
+            { name: '董事会', manger: '曹操' }
+          ]
+        },
+        { name: '行政部', manger: '曹丕' },
+        { name: '人事部', manger: '曹贼' }
+      ],
+      defaultProps: {
+        label: 'name'
+      }
+    }
+  }
 }
+
 </script>
 
-<style>
-
+<style scoped>
+.tree-card{
+padding: 30px 40px;
+font-size: 14px;
+}
 </style>
